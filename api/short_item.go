f47eae4a93426afa9536a9bc23420bf44748678e
swisscheesy <47891780+swisscheesy@users.noreply.github.com>
2026-03-16 21:41:16 -0700

    chore: cleanup 2 older unused files

--- api/short_item.go
+++
@@ -1,9 +0,0 @@
-package api
-
-type ShortItem struct {
-	ItemName string `json:"item_name" alias:"niinlookup.niin"`
-	Niin     string `json:"niin" alias:"niinlookup.niin"`
-	Fsc      string `json:"fsc" alias:"niinlookup.fsc"`
-	HasAmdf  bool   `json:"has_amdf" alias:"niinlookup.has_amdf"`
-	HasFlis  bool   `json:"has_flis" alias:"niinlookup.has_flis"`
-}
