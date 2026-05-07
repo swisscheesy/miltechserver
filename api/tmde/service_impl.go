41d0d1e9f112c4cd96373206c190d199310edf08
swisscheesy <47891780+swisscheesy@users.noreply.github.com>
2026-05-06 22:08:13 -0700

    feat(tmde): implement service

---
+++ api/tmde/service_impl.go
@@ -0,0 +1,24 @@
+package tmde
+
+import (
+	"strings"
+
+	"miltechserver/.gen/miltech_ng/public/model"
+)
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{repository: repo}
+}
+
+func (s *service) LookupByNIIN(niin string) (model.TmdeRequirements, error) {
+	normalized := strings.TrimSpace(strings.ToUpper(niin))
+	return s.repository.GetByNIIN(normalized)
+}
+
+func (s *service) GetAllPaginated(page int) (TmdePageResponse, error) {
+	return s.repository.GetAllPaginated(page)
+}
